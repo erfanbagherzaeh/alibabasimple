e83320e9f74e39e55c69f0eb5eadb473fb01ebd9
saydpishe <alirezasaydpishe20@gmail.com>
2024-11-04 14:34:18 +0330

    new commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -22,7 +22,7 @@ function Home(){
     } , [])
     return(
         <>
-        {/* <Navbar></Navbar>
+        <Navbar></Navbar>
         <div className=" w-full  mb-5 sm:h-56 sm:w-4/5 sm:m-auto">
             <img className="z-0 rounded-3xl w-full " src="https://cdn.alibaba.ir/h2/desktop/assets/images/hero/hero-e1fa22fb.webp"/>
         </div>
@@ -149,8 +149,8 @@ function Home(){
         <Intro></Intro>
         <Footer></Footer>
 
-         */}
-        <Login></Login>
+        
+        {/* <Login></Login> */}
         </>
     )
 }
